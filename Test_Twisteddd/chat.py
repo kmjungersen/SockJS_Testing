601795caaea9323b4aa0cd8e17dda080425c0536
Kurtis Jungersen <kmjungersen@gmail.com>
2014-05-30 14:01:03 -0400

    Minor Structure Changes

---
+++ Test_Twisteddd/chat.py
@@ -0,0 +1,15 @@
+from twisted.internet import reactor, protocol, ssl
+from twisted.web import static, server, resource
+import twisted.web
+
+from OpenSSL import SSL
+from txsockjs.factory import SockJSResource
+
+class IndexHandler(twisted)
+
+
+
+
+
+
+
